ff29d4e7711f86d97859039cdb857f902d722985
Estuardo José Estrada <estuardo.ej@alliedglobalbpo.com>
2020-09-17 16:12:57 -0600

    Static container with nginx

---
+++ static/js/funciones.js
@@ -0,0 +1,17 @@
+
+
+
+function accion1()
+{
+    document.querySelector("#cards1").classList.add('hide');
+}
+
+function accion2()
+{
+    document.querySelector("#cards2").classList.add('hide');
+}
+
+function accion3()
+{
+    document.querySelector("#cards3").classList.add('hide');
+}
